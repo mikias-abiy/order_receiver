7b833158df02db60b7920a377b630272da50eee9
Mikias <mikias_abiy@proton.me>
2024-03-31 09:51:43 -0400

    update: models/__init.py: user_setting manager created and reloaded

--- models/__init__.py
+++ models/__init__.py
@@ -5,9 +5,13 @@ from dotenv  import load_dotenv
 
 from models.engine import file_storage as fs
 from models.manager import order_manager as om
+from models.manager import user_setting_manager as usm
 
 storage = fs.FileStorage()
 storage.reload()
 
 order_manager = om.OrderManager()
 order_manager.reload()
+
+user_setting_manager = usm.UserSettingManager()
+user_setting_manager.reload()
